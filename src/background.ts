d79e0f6e4b5d527f39e862b842e55a80788dedba
Victor <victorwu@users.noreply.github.com>
2018-01-18 03:50:13 -0800

    cleaned background.ts

--- src/background.ts
+++ src/background.ts
@@ -2,19 +2,15 @@ import * as Web3 from 'web3';
 
 let blocksBehind = 0;
 
-
 function polling() {
-  console.log('polling, blocksBehind: ' + blocksBehind);
-
   // Check ETH network every 30 seconds
   chrome.storage.sync.get(null, function(items) {
     var usingRPC;
     if(!items.rpcProvider || items.rpcProvider == undefined) {
       usingRPC = 'https://mainnet.infura.io/radar';
     } else usingRPC = items.rpcProvider;
-    console.log("RPC initialized to: " + usingRPC);
-
     var web3 = new Web3(new Web3.providers.HttpProvider(usingRPC));
+
     blocksBehind = web3.eth.blockNumber - items.bnum;
   });
 
@@ -23,6 +19,7 @@ function polling() {
     chrome.browserAction.setBadgeText({text: '' + blocksBehind}); 
   }
 
+  console.log('polling, blocksBehind: ' + blocksBehind);
   setTimeout(polling, 1000 * 30);
 }
 
